79849111a2c7013f4740ca6188edaed48c1053e3
Jean Montero <jean@IE0117.myguest.virtualbox.org>
2024-05-01 20:06:35 -0600

    Se noto que habia un int mal puesto y se cambio la segunda parte del codigo para que reciba un numero del usuario

--- ej2.c
+++ ej2.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
+#include <stdio.h> //agregar ya que se quiere usar printf y scanf
 
 int factorial  (int n)  {
     int  i =  1;
     while (n > 1) {
     	i = i * n;
-    	int n = n - 1;
+    	n = n - 1; //hay que quitar el int porque se esta creando una variable n dentro del bucle, esto hace que n no se actualice
     }
     return  i;
 }
 
-int main ( int argc , char *argv [ ] ) {
-   int fac4 = factorial (4);
-   int fac5 = factorial (5);
-   printf ("4! = %d, 5 ! = %d\n", fac4, fac5);
-   return 0;
+int main(int argc, char *argv[]) {
+    int numero; //se necesita para guardar lo que digite el usuario
+    printf("Ingrese un número para calcular su factorial: ");//aqui se le pide al usuario el numero que se va a guardar en lo anterior
+    scanf("%d", &numero); //leyendo el numero
+    
+    int resultado = factorial(numero); //aqui se usa la parte de factorial que se corrigio para luego guardar el resultado en la variable "resultado"
+    printf("%d! = %d\n", numero, resultado); //se imprime el calculo
+    
+    return 0;
 }
