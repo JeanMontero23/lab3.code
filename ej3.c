e2df040203834cfc4fe8bc417eb7c87f9d6cad51
Jean Montero <jean@IE0117.myguest.virtualbox.org>
2024-05-06 23:08:46 -0600

    Se revisa de nuevo la primera parte del codigo, la cual incluye las iteraciones. Ademas se agrega la seccion en la que se solicita un numero al usuario y comtempla las situaciones solicitadas. Todo parece funcionar bien, se puede hacer merge

--- ej3.c
+++ ej3.c
@@ -1,5 +1,6 @@
 #include <stdio.h> // se necesita para utilizar las funciones printf y scanf
 #include <stdlib.h> // se necesita para utilizar funciones que generen numeros aleatorios, se usa rand y srand
+#include <time.h> // se necesita para utilzar time()
 
 #define MAX_SIZE 10
 
@@ -54,4 +55,38 @@ void printMatrix(int matrix[][MAX_SIZE], int size) {
         }
         printf("\n");
     }
+}
+
+/*
+ *Primero se solicita un numero para las dimensiones de la matriz
+ *Se revisa si el numero esta entre 1 y 10, en caso de que no lo sea, se utilizara el valor prefijado igual a cinco
+ *Se crea la matriz y se rellena con valores binarios aleatorios mediante el uso de rand()
+ *Se imprime la matriz obtenida y se usa finLargestSquare() para encontrar el cuadrado mas grande que fue generado
+*/
+
+int main() {
+    int n;
+    printf("Ingrese un numero para las filas de la matriz nxn (entre 1 y 10): ");
+    scanf("%d", &n);
+
+    if (n < 1 || n > 10) {
+        printf("Número no válido. Se utilizará un valor por defecto.\n");
+        n = 5;
+    }
+
+    int matrix[MAX_SIZE][MAX_SIZE];
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = rand() % 2;
+        }
+    }
+
+    printf("La matriz utilizada corresponde a:\n");
+    printMatrix(matrix, n);
+
+    int largestSquareSize = findLargestSquare(matrix, n);
+    printf("El tamaño del cuadrado más grande de unos es: %dx%d.\n", largestSquareSize, largestSquareSize);
+
+    return 0;
 }
